1da52ea97a8115b42947d209768c40e8db81263a
tatt61880 <tatt61880@gmail.com>
2018-01-11 11:09:24 +0900

    .

--- _test/test.rb
+++ _test/test.rb
@@ -6,17 +6,19 @@ curDir = Dir.pwd
 Dir::chdir("..")
 dir = Dir.pwd
 
+tempFilename = "__temp__.txt"
+tempFilepath = "#{curDir}/#{tempFilename}"
+
 countOk = 0
 countUnexpected = 0
 Find.find(dir) {|fpath|
 	Find.prune if(fpath == curDir)
 	if fpath =~ /main.kn$/
-		outputFile = curDir + "/output.txt"
 		knDir = File::dirname(fpath)
 		out, err, status = Open3.capture3("cmd.exe /Q /C \"kuincl -i #{fpath} -e cui -q")
 		if status == 0
-			out, err, status = Open3.capture3("#{knDir}/out.exe > #{outputFile}")
-			if FileUtils.cmp(outputFile, "#{knDir}/output.txt")
+			out, err, status = Open3.capture3("#{knDir}/out.exe > #{tempFilepath}")
+			if FileUtils.cmp(tempFilepath, "#{knDir}/output.txt")
 				countOk += 1
 			else
 				puts "Error: [#{fpath.sub(dir, "")}]"
@@ -28,6 +30,7 @@ Find.find(dir) {|fpath|
 		end
 	end
 }
+File.unlink tempFilepath
 
 puts "#{countOk}/#{countOk + countUnexpected}"
 if countUnexpected == 0
@@ -35,3 +38,5 @@ if countUnexpected == 0
 else
 	puts "Count for unexpected result = #{countUnexpected}."
 end
+
+system("pause")
